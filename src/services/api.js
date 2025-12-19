e57b3d4dfad01a8675664278f654a0278b8c670f
MOSAWAF <92247135+MOSAWAF@users.noreply.github.com>
2025-12-19 06:28:59 +0300

    first commit

---
+++ src/services/api.js
@@ -0,0 +1,17 @@
+const API_KEY = "b939e349d432d223bd24aca1dedd329a";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
